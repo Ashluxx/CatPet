b40fadc9ca8955400555fff107cdf6948da91aec
Ashluxx <yyukemust@gmail.com>
2025-12-08 14:55:57 -0500

    built cat builder to solve the long list of inputs in te CAT.java

---
+++ src/main/java/CatPet/CatBuilder.java
@@ -0,0 +1,45 @@
+package CatPet;
+
+//Builder pattern is used to solve the long list of inputs (parameters) in the Cat constructor.
+public class CatBuilder {
+
+    String name;
+    String breed;
+    String eyecolor;
+    String haircolor;
+    String gender;
+    int age;
+
+    public CatBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public CatBuilder breed(String breed) {
+        this.breed = breed;
+        return this;
+    }
+
+    public CatBuilder eyecolor(String eyecolor) {
+        this.eyecolor = eyecolor;
+        return this;
+    }
+
+    public CatBuilder haircolor(String haircolor) {
+        this.haircolor = haircolor;
+        return this;
+    }
+
+    public CatBuilder gender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public CatBuilder age(int age) {
+        this.age = age;
+        return this;
+    }
+    public Cat createCat() {
+        return new Cat(name, breed, eyecolor, haircolor, gender, age);
+    }
+}
