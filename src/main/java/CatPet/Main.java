671120d61bf0d2f07db442dbc9f551c69dd10b7d
Ashluxx <yyukemust@gmail.com>
2025-12-08 15:14:08 -0500

    Replace cat built methond in main.java

--- src/main/java/CatPet/Main.java
+++ src/main/java/CatPet/Main.java
@@ -2,10 +2,18 @@ package CatPet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Run Succesfully");
 
-        Cat myCat = new Cat("Coco","Ragdoll",
-                "Blue", "White", "Male", 3);
+        /*Cat myCat = new Cat("Coco","Ragdoll",
+                "Blue", "White", "Male", 3);*/
+
+        Cat myCat = new CatBuilder()
+                .age(3)
+                .breed("Ragdoll")
+                .eyecolor("Blue")
+                .gender("Male")
+                .haircolor("White")
+                .name("Coco")
+                .createCat();
 
         myCat.meow();
     }
